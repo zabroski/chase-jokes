15ba34d3757a3e14e4424c3758c4d9f24618810f
zabroski <35234595+zabroski@users.noreply.github.com>
2021-04-06 09:59:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Jokes from './component/Jokes';
+
+function App() {
+  return (
+    <div className="App">
+      <Jokes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Jokes.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+const Jokes = () => {
+    return (
+        <div>
+            <h1>Jokes generator for Chase</h1>
+            <div>
+            <p class="joke">Hey there, click the button to generate a joke</p>
+            <button>Tell me a Joke</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default Jokes
+
+
